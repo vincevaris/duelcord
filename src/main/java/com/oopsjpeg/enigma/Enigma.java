8bb26fe1868a2f1ea7e780aa403e68087bcf8d1f
BuildTools <oopsjpeg@gmail.com>
2020-03-29 16:20:42 -0400

    Add lombok and move build methods to build command

--- src/main/java/com/oopsjpeg/enigma/Enigma.java
+++ src/main/java/com/oopsjpeg/enigma/Enigma.java
@@ -4,9 +4,6 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.oopsjpeg.enigma.game.Game;
 import com.oopsjpeg.enigma.game.GameMode;
-import com.oopsjpeg.enigma.game.Stats;
-import com.oopsjpeg.enigma.game.obj.Item;
-import com.oopsjpeg.enigma.game.obj.Unit;
 import com.oopsjpeg.enigma.listener.CommandListener;
 import com.oopsjpeg.enigma.listener.ReadyListener;
 import com.oopsjpeg.enigma.storage.Player;
@@ -20,7 +17,7 @@ import discord4j.core.object.entity.Guild;
 import discord4j.core.object.entity.TextChannel;
 import discord4j.core.object.entity.User;
 import discord4j.core.object.util.Snowflake;
-import discord4j.core.spec.EmbedCreateSpec;
+import lombok.Getter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,12 +25,13 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class Enigma {
@@ -41,21 +39,17 @@ public class Enigma {
     public static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
     public static final ScheduledExecutorService SCHEDULER = Executors.newScheduledThreadPool(Runtime.getRuntime().availableProcessors() + 1);
 
-    private static Enigma instance;
+    @Getter private static Enigma instance;
 
-    private MongoManager mongo;
-    private DiscordClient client;
-    private Settings settings = new Settings(getSettingsFile());
-    private ArrayList<Listener> listeners = new ArrayList<>();
+    @Getter private MongoManager mongo;
+    @Getter private DiscordClient client;
+    @Getter private Settings settings = new Settings(getSettingsFile());
+    @Getter private ArrayList<Listener> listeners = new ArrayList<>();
 
-    private CommandListener commands;
-    private LinkedList<Game> games = new LinkedList<>();
-    private HashMap<Long, Player> players = new HashMap<>();
-    private HashMap<GameMode, LinkedList<Player>> queues = new HashMap<>();
-
-    public static Enigma getInstance() {
-        return instance;
-    }
+    @Getter private CommandListener commands;
+    @Getter private LinkedList<Game> games = new LinkedList<>();
+    @Getter private HashMap<Long, Player> players = new HashMap<>();
+    @Getter private HashMap<GameMode, LinkedList<Player>> queues = new HashMap<>();
 
     public static File getSettingsFile() {
         return new File("enigma.properties");
@@ -96,10 +90,6 @@ public class Enigma {
         } catch (IOException error) {
             error.printStackTrace();
         }
-    }
-
-    public CommandListener getCommands() {
-        return commands;
     }
 
     public void addListener(Listener listener) {
@@ -200,46 +190,6 @@ public class Enigma {
         SCHEDULER.schedule(() -> game.getChannel().delete().block(), 2, TimeUnit.MINUTES);
     }
 
-    public void buildUnitsChannel() {
-        System.out.println("Building units channel.");
-        Arrays.stream(Unit.values()).forEach(u -> getUnitsChannel().createEmbed(e -> {
-            e.setTitle("**" + u.getName() + "**");
-            e.setColor(u.getColor());
-            String desc = "Health: **" + u.getStats().getInt(Stats.MAX_HEALTH) + "** (+**" + u.getStats().getInt(Stats.HEALTH_PER_TURN) + "**/turn)"
-                    + "\nDamage: **" + u.getStats().getInt(Stats.DAMAGE) + "**"
-                    + "\nEnergy: **" + u.getStats().getInt(Stats.ENERGY) + "**";
-            if (u.getStats().get(Stats.CRIT_CHANCE) > 0)
-                desc += "\nCritical Chance: **" + Util.percent(u.getStats().get(Stats.CRIT_CHANCE)) + "**";
-            if (u.getStats().get(Stats.LIFE_STEAL) > 0)
-                desc += "\nLife Steal: **" + Util.percent(u.getStats().get(Stats.LIFE_STEAL)) + "**";
-            e.setDescription(desc);
-            e.addField("Passives / Abilities", u.getDescription(), false);
-        }).block());
-    }
-
-    public Consumer<EmbedCreateSpec> buildItemTree(Item.Tree tree) {
-        return e -> {
-            e.setTitle("**" + tree.getName() + "**");
-            e.setColor(tree.getColor());
-            Item.fromTree(tree).stream()
-                    .sorted(Comparator.comparingInt(Item::getCost))
-                    .forEach(i -> {
-                        String value = (i.hasTip() ? "_" + i.getTip() + "_\n" : "")
-                                + Util.formatStats(i.getStats()) + "\n"
-                                + (i.hasBuild() ? "[_" + Arrays.stream(i.getBuild()).map(Item::getName).collect(Collectors.joining(", ")) + "_]" : "");
-                        e.addField(i.getName() + " (" + i.getCost() + "g)", value, true);
-                    });
-        };
-    }
-
-    public void buildItemsChannel() {
-        System.out.println("Building items channel.");
-        getItemsChannel().createEmbed(buildItemTree(Item.Tree.CONSUMABLES)).block();
-        getItemsChannel().createEmbed(buildItemTree(Item.Tree.DAMAGE)).block();
-        getItemsChannel().createEmbed(buildItemTree(Item.Tree.HEALTH)).block();
-        getItemsChannel().createEmbed(buildItemTree(Item.Tree.ABILITY)).block();
-    }
-
     public Guild getGuild() {
         return client.getGuildById(Snowflake.of(settings.get(Settings.GUILD_ID))).block();
     }
@@ -258,29 +208,5 @@ public class Enigma {
 
     public TextChannel getLogChannel() {
         return client.getChannelById(Snowflake.of(settings.get(Settings.LOG_ID))).cast(TextChannel.class).block();
-    }
-
-    public MongoManager getMongo() {
-        return mongo;
-    }
-
-    public DiscordClient getClient() {
-        return client;
-    }
-
-    public Settings getSettings() {
-        return settings;
-    }
-
-    public List<Listener> getListeners() {
-        return listeners;
-    }
-
-    public List<Game> getGames() {
-        return games;
-    }
-
-    public Map<Long, Player> getPlayers() {
-        return players;
     }
 }
--- src/main/java/com/oopsjpeg/enigma/GeneralCommand.java
+++ src/main/java/com/oopsjpeg/enigma/GeneralCommand.java
@@ -1,29 +1,80 @@
 package com.oopsjpeg.enigma;
 
 import com.oopsjpeg.enigma.game.GameMode;
+import com.oopsjpeg.enigma.game.Stats;
+import com.oopsjpeg.enigma.game.obj.Item;
+import com.oopsjpeg.enigma.game.obj.Unit;
 import com.oopsjpeg.enigma.storage.Player;
 import com.oopsjpeg.enigma.util.Util;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.MessageChannel;
+import discord4j.core.object.entity.TextChannel;
 import discord4j.core.object.entity.User;
 import discord4j.core.object.util.Permission;
 import discord4j.core.object.util.PermissionSet;
+import discord4j.core.spec.EmbedCreateSpec;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+import static com.oopsjpeg.enigma.game.Stats.*;
 
 public enum GeneralCommand implements Command {
     BUILD("build") {
         @Override
         public void execute(Message message, String alias, String[] args) {
-            if (args[0].equalsIgnoreCase("items"))
-                Enigma.getInstance().buildItemsChannel();
-            else if (args[0].equalsIgnoreCase("units"))
-                Enigma.getInstance().buildUnitsChannel();
+            if (args[0].equalsIgnoreCase("items")) {
+                TextChannel channel = Enigma.getInstance().getItemsChannel();
+                channel.bulkDelete(c -> channel.getMessagesAfter(channel.getId()));
+                channel.createEmbed(buildItemTree(Item.Tree.CONSUMABLES)).block();
+                channel.createEmbed(buildItemTree(Item.Tree.DAMAGE)).block();
+                channel.createEmbed(buildItemTree(Item.Tree.HEALTH)).block();
+                channel.createEmbed(buildItemTree(Item.Tree.ABILITY)).block();
+            } else if (args[0].equalsIgnoreCase("units")) {
+                TextChannel channel = Enigma.getInstance().getUnitsChannel();
+                channel.bulkDelete(c -> channel.getMessagesAfter(channel.getId()));
+                for (Unit unit : Unit.values()) {
+                    channel.createEmbed(embed -> {
+                        embed.setTitle(unit.getName());
+                        embed.setColor(unit.getColor());
+                        List<String> desc = new ArrayList<>();
+                        Stats stats = unit.getStats();
+                        desc.add("Health: **" + stats.getInt(MAX_HEALTH) + "** (+**" + stats.getInt(HEALTH_PER_TURN) + "**/turn)");
+                        desc.add("Damage: **" + stats.getInt(DAMAGE) + "**");
+                        desc.add("Energy: **" + stats.getInt(ENERGY) + "**");
+                        if (stats.get(CRIT_CHANCE) > 0)
+                            desc.add("Critical Chance: **" + Util.percent(stats.get(CRIT_CHANCE)) + "**");
+                        if (stats.get(LIFE_STEAL) > 0)
+                            desc.add("Life Steal: **" + Util.percent(stats.get(LIFE_STEAL)) + "**");
+                        embed.setDescription(String.join("\n", desc));
+                        embed.addField("Passives / Abilities", unit.getDescription(), false);
+                    }).block();
+                }
+            }
         }
 
         @Override
         public PermissionSet getPermissions() {
             return PermissionSet.of(Permission.MANAGE_GUILD);
+        }
+
+        public Consumer<EmbedCreateSpec> buildItemTree(Item.Tree tree) {
+            return e -> {
+                e.setTitle("**" + tree.getName() + "**");
+                e.setColor(tree.getColor());
+                Item.fromTree(tree).stream()
+                        .sorted(Comparator.comparingInt(Item::getCost))
+                        .forEach(i -> {
+                            String value = (i.hasTip() ? "_" + i.getTip() + "_\n" : "")
+                                    + Util.formatStats(i.getStats()) + "\n"
+                                    + (i.hasBuild() ? "[_" + Arrays.stream(i.getBuild()).map(Item::getName).collect(Collectors.joining(", ")) + "_]" : "");
+                            e.addField(i.getName() + " (" + i.getCost() + "g)", value, true);
+                        });
+            };
         }
     },
     QUEUE("queue", "find", "mm", "q") {
